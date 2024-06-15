f48d3547f0ca781f475aaa1d7c22023b4131f3bc
Rodrigo Alejandro González <61030017+RodrigoGonzalez78@users.noreply.github.com>
2024-06-15 00:31:58 -0300

    Update datos.sql

--- src/datos.sql
+++ src/datos.sql
@@ -2,27 +2,13 @@
 --Insertar los roles en el siguiente orden 
 
 -- Insertar el rol de Administrador
-INSERT INTO Rol (nombre, rol_inactivo)
+INSERT INTO Rol (nombre)
 VALUES ('Administrador');
 
 -- Insertar el rol de Instructor
-INSERT INTO Rol (nombre, rol_inactivo)
+INSERT INTO Rol (nombre)
 VALUES ('Instructor');
 
 -- Insertar el rol de Cliente
-INSERT INTO Rol (nombre, rol_inactivo)
+INSERT INTO Rol (nombre)
 VALUES ('Cliente');
-
---Nuevos Usuarios ejemplo
-
--- Insertar un usuario Administrador
-INSERT INTO Usuario (id_rol, apodo, nombre, apellido, avatar_url, contrasena, fecha_creacion, numero_telefono, sexo, usuario_inactivo)
-VALUES (1, 'admin', 'AdminNombre', 'AdminApellido', 'http://example.com/avatar_admin.png', 'admin_contrasena', GETDATE(), '1234567890', 'M', 0);
-
--- Insertar un usuario Instructor
-INSERT INTO Usuario (id_rol, apodo, nombre, apellido, avatar_url, contrasena, fecha_creacion, numero_telefono, sexo, usuario_inactivo)
-VALUES (2, 'instr', 'InstrNombre', 'InstrApellido', 'http://example.com/avatar_instr.png', 'instr_contrasena', GETDATE(), '0987654321', 'F', 0);
-
--- Insertar un usuario Cliente
-INSERT INTO Usuario (id_rol, apodo, nombre, apellido, avatar_url, contrasena, fecha_creacion, numero_telefono, sexo, usuario_inactivo)
-VALUES (3, 'cliente', 'ClienteNombre', 'ClienteApellido', 'http://example.com/avatar_cliente.png', 'cliente_contrasena', GETDATE(), '1122334455', 'M', 0);
